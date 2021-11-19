a413d8c11f18cd90f37c0ac0a972f333f64be1c7
cms7 <cms7@hw.ac.uk>
2021-11-19 12:46:07 +0000

    push

--- PSO.py
+++ PSO.py
@@ -1,67 +1,41 @@
-
 from Particle import Particle
 import numpy as np
+from Particle import find_best
+from functions import assess_fitness
 from random import seed
 
-def get_best_possible_fitness(problem):
-    optimal_solutions = problem.get_optimal_solutions()
-    an_optimal_solution = optimal_solutions[0]
-    problem.evaluate(an_optimal_solution)
-    return an_optimal_solution.objective_values
-
 class PSO:
-    def __init__(self, problem, swarm_size, vector_length, num_informants=2):
-        self.swarm_size = swarm_size
+
+    def __init__(self,swarmsize,dimensions,num_informants):
+        self.swarmsize=swarmsize
         self.num_informants = num_informants
-        self.problem = problem
-        self.swarm = [Particle(self.problem, np.random.rand(vector_length), np.random.rand(vector_length))
-                      for x in range(swarm_size)]
-        self.global_fittest = np.random.choice(self.swarm, 1)[0]
-        
-    def find_current_best(self, swarm):
-        """Evaluates a given swarm and returns the fittest particle based on their best previous position"""
-        fitnesses = [Particle.evaluate_position(x.fittest_position, self.problem) for x in swarm]
-        best_value = min(fitnesses)
-        best_index = fitnesses.index(best_value)
-        return swarm[best_index], fitnesses
+        self.swarm = [Particle( np.zeros(dimensions), np.random.rand(
+            dimensions), i) for i, x in enumerate(range(swarmsize))]
+        self.g_best = np.random.choice(self.swarm,1)[0]
     
     def update_swarm(self):
-        """Update each particle in the swarm"""
         for particle in self.swarm:
-            informants = np.random.choice(self.swarm, self.num_informants)
-            if (particle not in informants):
-                np.append(informants, particle)
-            fittest_informant, _ = self.find_current_best(informants)
-            
-            particle.update(fittest_informant, self.global_fittest)
-    
-    def improve(self, num_iterations=200):
-        """Improves the population for a given number of iterations.
+            informants = np.random.choice(self.swarm,self.num_informants)
+            if particle not in informants:
+                np.append(informants,particle)
+            i_best = find_best(informants)
+            particle.update(i_best,self.g_best)
 
-        Parameters
-        ----------
-        num_iterations : int, optional
-            The number of iterations before stopping improving
-        
-        Returns
-        -------
-        results : list
-            A list of the best fitness at each generation
-        found_global_min_at : int
-            The position at which the global min was found at, return -1 if not found
-        """
-        best_possible_fitness = get_best_possible_fitness(self.problem)
-        fitnesses = []
-        found_global_min_at = -1
-        for i in range(num_iterations):
-            fittest, _ = self.find_current_best(self.swarm)
-            global_fittest_fitness = self.global_fittest.assess_fitness()
-            if (fittest.assess_fitness() < global_fittest_fitness):
-                self.global_fittest = fittest
-            fitnesses.append(global_fittest_fitness)
+    def update_gbest(self):
+        most_fit = find_best(self.swarm)
+        g_best_fitness = self.g_best.assess_fitness()
+        if(most_fit.assess_fitness()<g_best_fitness):
+            self.g_best = most_fit
+
+    def improve(self, epochs):
+        for i in range(epochs):
             self.update_swarm()
-            if math.isclose(best_possible_fitness, global_fittest_fitness, rel_tol=1e-1) :
-                if found_global_min_at == -1:
-                    found_global_min_at = i
-        return fitnesses, found_global_min_at
-
+            self.update_gbest()
+            B = assess_fitness(self.g_best.position)
+            X = self.g_best.position[0]
+            Y = self.g_best.position[1]
+            print('Epoch = %i, Y = %f, X = %f ' % (i,X,Y))
+    
+seed(1)
+pso = PSO(100,2,10)
+pso.improve(100)
--- Particle.py
+++ Particle.py
@@ -38,36 +38,3 @@ def find_best(swarm):
     fittest_particle = fitnesses.index(most_fit)
     return swarm[fittest_particle]
 
-class PSO:
-
-    def __init__(self,swarmsize,dimensions,num_informants):
-        self.swarmsize=swarmsize
-        self.num_informants = num_informants
-        self.swarm = [Particle( np.zeros(dimensions), np.random.rand(
-            dimensions), i) for i, x in enumerate(range(swarmsize))]
-        self.g_best = np.random.choice(self.swarm,1)[0]
-    
-    def update_swarm(self):
-        for particle in self.swarm:
-            informants = np.random.choice(self.swarm,self.num_informants)
-            if particle not in informants:
-                np.append(informants,particle)
-            i_best = find_best(informants)
-            particle.update(i_best,self.g_best)
-
-    def update_gbest(self):
-        most_fit = find_best(self.swarm)
-        g_best_fitness = self.g_best.assess_fitness()
-        if(most_fit.assess_fitness()<g_best_fitness):
-            self.g_best = most_fit
-
-    def improve(self, epochs):
-        for i in range(epochs):
-            self.update_swarm()
-            self.update_gbest()
-            X = self.g_best.position[0]
-            Y = self.g_best.position[1]
-        print('Y = %f, X = %f' % (X, Y))
-
-pso = PSO(50,2,10)
-pso.improve(100)
